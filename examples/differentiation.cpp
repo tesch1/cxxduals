cdd656502e650c80a11953557b5f5c55ece5546c
michael tesch <tesch1@gmail.com>
2015-03-03 01:20:54 +0100

    fixed initializers and printing and finished part access

--- examples/differentiation.cpp
+++ examples/differentiation.cpp
@@ -42,10 +42,14 @@ int main(int argc, char **argv)
   typedef dual<dual<double> > HDUALTYPE;
   HDUALTYPE a(DUALTYPE(1,2),DUALTYPE(3,4));
   HDUALTYPE b(DUALTYPE(11,12),DUALTYPE(13,14));
+  hyperdualcd c(DUALTYPE(1,2),DUALTYPE(3,4));
+  hyperdualcd d{1,2,3,{4,4}};
   std::cout << a << "\n"
             << b << "\n"
             << a+b << "\n"
-            << a*b << "\n";
+            << a*b << "\n"
+            << c << "\n"
+            << d << "\n";
 
   std::cout << "double<> depth:" << dual_trait_helper<double>::depth
             << " num_elem: " << dual_trait_helper<double>::num_elem
